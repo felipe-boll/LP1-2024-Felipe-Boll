c128cefa6b455c372d397764c39b0729519e8d05
Felipe Boll <felipechavesboll@gmail.com>
2024-09-15 18:39:13 -0300

    Adicionado um sout depois do for

--- 2024/LP/first/src/main/java/ex/felipeBoll_listaB_02/FelipeBoll_1151.java
+++ 2024/LP/first/src/main/java/ex/felipeBoll_listaB_02/FelipeBoll_1151.java
@@ -1,12 +1,35 @@
 package ex.felipeBoll_listaB_02;
 
+import java.util.Scanner;
+
 public class FelipeBoll_1151 {
 
   public static void fibonacci() {
+    Scanner tecladoScanner = new Scanner(System.in);
 
+    int primeiroTermo = 0;
+    int segundoTermo = 1;
+    int termoSeguinte;
+
+    int casaFibonacci = tecladoScanner.nextInt();
+
+    System.out.printf("%d %d", primeiroTermo, segundoTermo);
+
+    for (int i = 3; i <= casaFibonacci; i++) {
+      termoSeguinte = primeiroTermo + segundoTermo;
+      System.out.printf(" %d", termoSeguinte);
+
+      primeiroTermo = segundoTermo;
+      segundoTermo = termoSeguinte;
+
+    }
+
+    System.out.println();
+
+    tecladoScanner.close();
   }
 
   public static void main(String[] args) {
-
+    fibonacci();
   }
 }
