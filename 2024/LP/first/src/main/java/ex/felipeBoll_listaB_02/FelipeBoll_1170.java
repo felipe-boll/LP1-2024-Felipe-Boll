99ff1cb9f678e7f6a7bca7c4b30c8e7872826bbf
Felipe Boll <felipechavesboll@gmail.com>
2024-09-23 18:53:12 -0300

    \n no printf depois do for

--- 2024/LP/first/src/main/java/ex/felipeBoll_listaB_02/FelipeBoll_1170.java
+++ 2024/LP/first/src/main/java/ex/felipeBoll_listaB_02/FelipeBoll_1170.java
@@ -3,36 +3,26 @@ package ex.felipeBoll_listaB_02;
 import java.util.Scanner;
 
 public class FelipeBoll_1170 {
-    
-    public static void descobrirDuracaoDaComidaDosBlobs(){
+
+    public static void descobrirDuracaoDaComidaDosBlobs() {
         Scanner tecladoScanner = new Scanner(System.in);
 
-        // quantidade de dias que a comida vai durar
-        int diasDaDuracao = 0;
+        int quantTestes = tecladoScanner.nextInt();
 
-        int quantidadeTestes = tecladoScanner.nextInt();
+        for (int i = 0; i < quantTestes; i++) {
+            double quantInicialDeComida = tecladoScanner.nextDouble();
+            int dias = 0;
 
-        for(int i = 1; i <= quantidadeTestes; i++){
-
-            double quantidadeDeComidaDisponivel = tecladoScanner.nextDouble();
-
-            while (true) {
-                if (quantidadeDeComidaDisponivel == 0) {
-                    System.out.printf("%d dias\n", diasDaDuracao);
-                    break;
-                }
-
-                diasDaDuracao++;
-                quantidadeDeComidaDisponivel /= (float) 2;
-
+            while (quantInicialDeComida > 1.0) {
+                quantInicialDeComida /= 2.0;
+                dias++;
             }
-
+            System.out.printf("%d dias\n", dias);
         }
 
         tecladoScanner.close();
     }
-    
-    
+
     public static void main(String[] args) {
         descobrirDuracaoDaComidaDosBlobs();
     }
