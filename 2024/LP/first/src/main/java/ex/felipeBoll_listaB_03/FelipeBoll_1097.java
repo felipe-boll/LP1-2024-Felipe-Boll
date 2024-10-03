40fbcaf0273413520beca5d27673b3fca7759557
felipe-boll <felipechavesboll@gmail.com>
2024-10-03 16:50:16 -0300

    Ajustes

--- 2024/LP/first/src/main/java/ex/felipeBoll_listaB_03/FelipeBoll_1097.java
+++ 2024/LP/first/src/main/java/ex/felipeBoll_listaB_03/FelipeBoll_1097.java
@@ -1,5 +1,18 @@
 package ex.felipeBoll_listaB_03;
 
 public class FelipeBoll_1097 {
+    
+    public static void descobrirIJ3() {
+        
+        int I = 1;
 
+        for(int j = 7; j >= )
+
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        descobrirIJ3();
+    }
 }
