5c234ba9213b4157c46f6444b3fe4aefa9bffe5e
felipe-boll <felipechavesboll>
2024-10-07 08:17:26 -0300

    Foi, mas não foi

--- 2024/LP/first/src/main/java/ex/felipeBoll_listaB_03/FelipeBoll_1097.java
+++ 2024/LP/first/src/main/java/ex/felipeBoll_listaB_03/FelipeBoll_1097.java
@@ -4,12 +4,14 @@ public class FelipeBoll_1097 {
     
     public static void descobrirIJ3(int I, int J) {
         
-        
+        int Jdef = J - 2;
 
         for(int i = I; i <= 9; i += 2){
-            for(int j = J; j > j - 2 ; j--){
+            
+            for(int j = J; j >= Jdef; j = Jdef){
                 System.out.printf("I=%d J=%d\n", i, j);
             }
+            int j = Jdef;
         }
             
         
