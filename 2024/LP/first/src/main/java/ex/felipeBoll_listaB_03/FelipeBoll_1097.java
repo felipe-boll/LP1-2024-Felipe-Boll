93e64d8cb9dd33e5a36941a2a45a730650943b11
felipe-boll <felipechavesboll>
2024-10-07 08:10:15 -0300

    tentativa de fazer a sequencia IJ3

--- 2024/LP/first/src/main/java/ex/felipeBoll_listaB_03/FelipeBoll_1097.java
+++ 2024/LP/first/src/main/java/ex/felipeBoll_listaB_03/FelipeBoll_1097.java
@@ -2,19 +2,28 @@ package ex.felipeBoll_listaB_03;
 
 public class FelipeBoll_1097 {
     
-    public static void descobrirIJ3() {
+    public static void descobrirIJ3(int I, int J) {
         
-        int i = 1;
+        
 
-        for(int j = 7; i <= 9; i--){
-            
+        for(int i = I; i <= 9; i += 2){
+            for(int j = J; j > j - 2 ; j--){
+                System.out.printf("I=%d J=%d\n", i, j);
+            }
         }
+            
+        
 
     }
     
-    //testando
     
     public static void main(String[] args) {
-        descobrirIJ3();
+        
+        int I = 1;
+        int J = 7;
+        
+        descobrirIJ3(I, J);
+
+
     }
 }
