96f9e32f8b503ac29754529b183f4d8568f0ea29
felipe-boll <felipechavesboll@gmail.com>
2024-10-08 11:57:36 -0300

    08/10

---
+++ 2024/LP/first/src/main/java/ex/felipeBoll_listaB_03/FelipeBoll_1099.java
@@ -0,0 +1,46 @@
+package ex.felipeBoll_listaB_03;
+
+import java.util.Scanner;
+
+public class FelipeBoll_1099 {
+
+    public static Scanner teladoScanner = new Scanner(System.in);
+
+    public static int descobrirMaior(int x, int y) {
+        int maior;
+
+        if (y > x) {
+            maior = y;
+        } else {
+            maior = x;
+        }
+
+        return maior;
+
+    }
+
+    public static int descobrirMenor(int x, int y){
+        int menor;
+
+        if ()
+
+    }
+
+    public static void descobrirSomaImparesCons(int quantTestes) {
+
+        for (int i = 0; i < quantTestes; i++) {
+            int x = teladoScanner.nextInt();
+            int y = teladoScanner.nextInt();
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int quantTestes = teladoScanner.nextInt();
+
+        descobrirSomaImparesCons(quantTestes);
+
+    }
+}
