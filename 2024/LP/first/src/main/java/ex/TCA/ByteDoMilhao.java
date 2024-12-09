71b8864bfbe763b0e88a6e84ad9d424ce96a154d
Felipe Boll <felipechavesboll@gmail.com>
2024-12-09 20:22:21 -0300

    TCA

--- 2024/LP/first/src/main/java/ex/TCA/ByteDoMilhao.java
+++ 2024/LP/first/src/main/java/ex/TCA/ByteDoMilhao.java
@@ -10,7 +10,7 @@ public class ByteDoMilhao {
     System.out.println("Qual seu nome");
     String nome = tecladoScanner.nextLine();
 
-    if (nome.equals("Odair")) {
+    if (nome.equals("Odair") || nome.equals("Oda")) {
       nome = "Mestre";
     }
 
@@ -25,8 +25,18 @@ public class ByteDoMilhao {
     }
   }
 
+  public static String[] patentesNaEmpresa() {
+    String[] patente = { "Estagiario" };
+
+    return patente;
+  }
+
   public static String[][] criarPerguntasEAlternativas() {
-    String[][] perguntasEAlternativas = { { "Teste", "A", "B", "C", "D" } };
+    String[][] perguntasEAlternativas = { { "Teste", "A", "B", "C", "D" },
+        { "Teste", "A", "B", "C", "D" },
+        { "Teste", "A", "B", "C", "D" },
+        { "Teste", "A", "B", "C", "D" },
+        { "Teste", "A", "B", "C", "D" } };
 
     return perguntasEAlternativas;
   }
@@ -37,27 +47,48 @@ public class ByteDoMilhao {
     return alternativasCorretas;
   }
 
-  public static void imprimirPerguntaEAlternativas(String[][] perguntasEAlternativas) {
-    int linhas = perguntasEAlternativas.length;
+  public static void imprimirPerguntaEAlternativas(String[][] perguntasEAlternativas, int numPerguntas) {
     int colunas = perguntasEAlternativas[0].length;
 
-    for (int i = 0; i < linhas; i++) {
-      for (int j = 0; j < colunas; j++) {
-        System.out.printf("%s\n%s\n%s\n%s\n%s\n", perguntasEAlternativas[i][j]);
-      }
+    for (int j = 0; j < colunas; j++) {
+      System.out.printf("%s\n", perguntasEAlternativas[numPerguntas][j]);
     }
+
   }
 
-  // Que porra ta acontecendo??
+  public static String perguntaDaAlternativa(String nome) {
+    System.out.printf("Então %s, qual vai escolher? A, B, C ou D?\n", nome);
+    String alternativaSelecionada = tecladoScanner.nextLine();
+
+    return alternativaSelecionada;
+  }
+
+  public static void verificarSePerguntaCorreta(String alternativaSelecionada, String[] alternativasCorretas,
+      int numPerguntas) {
+    if (alternativasCorretas[numPerguntas].equals(alternativaSelecionada)) {
+
+    }
+  }
 
   public static void main(String[] args) {
     String nome = qualSeuNome();
     falaInicial(nome);
     String[][] perguntasEAlternativas = criarPerguntasEAlternativas();
     String[] alternativasCorretas = criarAlternativasCorretas();
+    String[] patente = patentesNaEmpresa();
 
-    for (int i = 0; i < alternativasCorretas.length; i++) {
-      imprimirPerguntaEAlternativas(perguntasEAlternativas[i][]);
+    while (true) {
+      int numPerguntas = 0;
+
+      if (numPerguntas == perguntasEAlternativas.length) {
+        break;
+      }
+
+      imprimirPerguntaEAlternativas(perguntasEAlternativas, numPerguntas);
+
+      String alternativaSelecionada = perguntaDaAlternativa(nome);
+
+      numPerguntas++;
     }
   }
 }
