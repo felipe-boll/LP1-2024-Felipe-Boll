21ccfa8adc8ba0c88ddf81010d385c5f9fb835bf
felipe-boll <felipechavesboll@gmail.com>
2024-12-12 12:01:58 -0300

    TCA

--- 2024/LP/first/src/main/java/ex/TCA/ByteDoMilhao.java
+++ 2024/LP/first/src/main/java/ex/TCA/ByteDoMilhao.java
@@ -6,33 +6,61 @@ public class ByteDoMilhao {
 
   public static Scanner tecladoScanner = new Scanner(System.in);
 
-  public static String qualSeuNome() {
-    System.out.println("Qual seu nome");
-    String nome = tecladoScanner.nextLine();
+  public static String comoJogoFunciona(){
+    String mensagem = "O jogo funciona da seguinte maneira: Voce entrara no Reality Show do nosso apresentador Silício Santos, onde ele te perguntara coisas sobre informatica e voce devera responder conforme a alternativa correta, conforme voce acerta ira subir nas patentes da empresa Empresa, voce começa como um simples estagiario mas pode até se tornar o CEO, boa sorte!!\n";
 
-    if (nome.equals("Odair") || nome.equals("Oda")) {
+    return mensagem;
+  }
+
+  public static void menu(){
+    String mensagem = comoJogoFunciona();
+    
+    while (true) {
+      System.out.printf("1 - Para jogar Byte Do Milhão.\n2 - Como o jogo funciona\n");
+      int interacaoMenu = tecladoScanner.nextInt();
+      
+      if (interacaoMenu == 1) {
+        System.out.println("Então vamos jogar");
+        break;
+      } else{
+        System.out.print(mensagem);
+      }
+    }
+  }
+
+  public static String qualSeuNome() {
+    System.out.println("Qual seu nome?");
+    String nome = tecladoScanner.next().toLowerCase();
+
+    if (nome.equals("odair") || nome.equals("oda")) {
       nome = "Mestre";
-    } else if(nome.equals("Lo") || nome.equals("Thiago Lo")){
+    } else if(nome.equals("lo") || nome.equals("thiago lo")){
       nome = "Senhor Thiagolo";
     }
 
     return nome;
   }
 
-  public static void falaInicial(String nome) {
+  public static void falaInicial(String nome, String suaPatente) {
     if (nome.equals("Mestre")) {
-      System.out.printf("Vejamos se não temos um mestre entre nós. Acho que este teste sera facil para o senhor\n");
+      System.out.printf("Vejamos se não temos um mestre entre nós. Acho que este teste sera facil para o senhor mas mesmo assim vamos te colocar na posição de %s\n", suaPatente);
     } else if (nome.equals("Senhor Thiagolo")) {
-      System.out.printf("Vejamos se não estamos com ele, com o magnifico, o grande, o incrivel, o SENHOR dos senhores, %s. Vamos ver se voce é capaz deste desafio\n", nome);
+      System.out.printf("Vejamos se não estamos com ele, com o magnifico, o grande, o incrivel, o SENHOR dos senhores, %s. Vamos ver se voce é capaz deste desafio, vamos ver como o senhor se sai começando como %s\n", nome, suaPatente);
     }else{
-      System.out.printf("Então vamos testar suas habilidades %s\n", nome);
+      System.out.printf("Então vamos testar suas habilidades %s, voce começara na empresa como %s\n", nome, suaPatente);
     }
   }
 
   public static String[] patentesNaEmpresa() {
-    String[] patente = { "Estagiario", "Alguma coisa" };
+    String[] patente = { "Estagiario", "Alguma coisa", "BBB" };
 
     return patente;
+  }
+
+  public static String promover(String[] patente, int posicao){
+    String cargo = patente[0];
+    System.out.println(cargo);
+    return cargo;
   }
 
   public static String[][] criarPerguntasEAlternativas() {
@@ -70,10 +98,11 @@ public class ByteDoMilhao {
   }
 
   public static void verificarSePerguntaCorreta(String alternativaSelecionada, String[] alternativasCorretas,
-      int numPerguntas, String suaPatente, String patentes, String nome){
+      int numPerguntas, String suaPatente, String patentes, String nome, int posicao){
 
     if (alternativasCorretas[numPerguntas].equals(alternativaSelecionada)) {
-      //suaPatente = patentes[];
+      posicao++;
+      suaPatente = promover(patentes, posicao);
       System.out.printf("Parabens, voce realmente acertou %s. Agora eu vou te promover para a posicão de %s\n", nome, suaPatente);
     } else{
       System.out.printf("Que pena %s, mas não desanima, vamos para a proxima. Voce esta na patente %s\n", nome, suaPatente);
@@ -81,12 +110,14 @@ public class ByteDoMilhao {
   }
 
   public static void main(String[] args) {
+    menu();
+    String[] patentes = patentesNaEmpresa();
+    int posicao = 0;
+    String suaPatente = promover(patentes, posicao);
     String nome = qualSeuNome();
-    falaInicial(nome);
+    falaInicial(nome, suaPatente);
     String[][] perguntasEAlternativas = criarPerguntasEAlternativas();
     String[] alternativasCorretas = criarAlternativasCorretas();
-    String[] patentes = patentesNaEmpresa();
-    String suaPatente = patentes[0];
     
     int numPerguntas = 0;
 
@@ -101,6 +132,7 @@ public class ByteDoMilhao {
       verificarSePerguntaCorreta(alternativaSelecionada, alternativasCorretas, numPerguntas, suaPatente, suaPatente, nome);
 
       numPerguntas++;
+
     }
   }
 }
