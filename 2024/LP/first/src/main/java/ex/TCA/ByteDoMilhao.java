719fc13e1026f92cdf142fa648f503df380f3e91
Felipe Boll <felipechavesboll@gmail.com>
2024-12-15 21:34:49 -0300

    TCA

--- 2024/LP/first/src/main/java/ex/TCA/ByteDoMilhao.java
+++ 2024/LP/first/src/main/java/ex/TCA/ByteDoMilhao.java
@@ -6,23 +6,17 @@ public class ByteDoMilhao {
 
   public static Scanner tecladoScanner = new Scanner(System.in);
 
-  public static String comoJogoFunciona(){
-    String mensagem = "O jogo funciona da seguinte maneira: Voce entrara no Reality Show do nosso apresentador Silício Santos, onde ele te perguntara coisas sobre informatica e voce devera responder conforme a alternativa correta, conforme voce acerta ira subir nas patentes da empresa Empresa, voce começa como um simples estagiario mas pode até se tornar o CEO, boa sorte!!\n";
+  public static void menu() {
+    String mensagem = "O jogo funciona da seguinte maneira: Voce entrara no Reality Show do nosso apresentador Silício Santos, onde ele te perguntara coisas sobre informatica e voce devera responder conforme a alternativa correta, conforme voce acerta ira subir nas patentes da empresa Empresa, voce começa como um simples estagiario mas pode até se tornar o CEO, boa sorte!!\n\n";
 
-    return mensagem;
-  }
-
-  public static void menu(){
-    String mensagem = comoJogoFunciona();
-    
     while (true) {
       System.out.printf("1 - Para jogar Byte Do Milhão.\n2 - Como o jogo funciona\n");
       int interacaoMenu = tecladoScanner.nextInt();
-      
+
       if (interacaoMenu == 1) {
         System.out.println("Então vamos jogar");
         break;
-      } else{
+      } else {
         System.out.print(mensagem);
       }
     }
@@ -34,7 +28,7 @@ public class ByteDoMilhao {
 
     if (nome.equals("odair") || nome.equals("oda")) {
       nome = "Mestre";
-    } else if(nome.equals("lo") || nome.equals("thiago lo")){
+    } else if (nome.equals("lo") || nome.equals("thiago lo")) {
       nome = "Senhor Thiagolo";
     }
 
@@ -43,10 +37,14 @@ public class ByteDoMilhao {
 
   public static void falaInicial(String nome, String suaPatente) {
     if (nome.equals("Mestre")) {
-      System.out.printf("Vejamos se não temos um mestre entre nós. Acho que este teste sera facil para o senhor mas mesmo assim vamos te colocar na posição de %s\n", suaPatente);
+      System.out.printf(
+          "Vejamos se não temos um mestre entre nós. Acho que este teste sera facil para o senhor mas mesmo assim vamos te colocar na posição de %s\n",
+          suaPatente);
     } else if (nome.equals("Senhor Thiagolo")) {
-      System.out.printf("Vejamos se não estamos com ele, com o magnifico, o grande, o incrivel, o SENHOR dos senhores, %s. Vamos ver se voce é capaz deste desafio, vamos ver como o senhor se sai começando como %s\n", nome, suaPatente);
-    }else{
+      System.out.printf(
+          "Vejamos se não estamos com ele, com o magnifico, o grande, o incrivel, o SENHOR dos senhores, %s. Vamos ver se voce é capaz deste desafio, vamos ver como o senhor se sai começando como %s\n",
+          nome, suaPatente);
+    } else {
       System.out.printf("Então vamos testar suas habilidades %s, voce começara na empresa como %s\n", nome, suaPatente);
     }
   }
@@ -57,9 +55,9 @@ public class ByteDoMilhao {
     return patente;
   }
 
-  public static String promover(String[] patente, int posicao){
-    String cargo = patente[0];
-    System.out.println(cargo);
+  public static String promover(String[] patente, int posicao) {
+    String cargo = patente[posicao];
+    // System.out.println(cargo);
     return cargo;
   }
 
@@ -74,7 +72,7 @@ public class ByteDoMilhao {
   }
 
   public static String[] criarAlternativasCorretas() {
-    String[] alternativasCorretas = {"A", "B", "C", "D", "A"};
+    String[] alternativasCorretas = { "A", "B", "C", "D", "A" };
 
     return alternativasCorretas;
   }
@@ -92,20 +90,26 @@ public class ByteDoMilhao {
 
   public static String perguntaDaAlternativa(String nome) {
     System.out.printf("Então %s, qual vai escolher? A, B, C ou D?\n", nome);
-    String alternativaSelecionada = tecladoScanner.nextLine();
+    String alternativaSelecionada = tecladoScanner.next();
 
     return alternativaSelecionada;
   }
 
   public static void verificarSePerguntaCorreta(String alternativaSelecionada, String[] alternativasCorretas,
-      int numPerguntas, String suaPatente, String patentes, String nome, int posicao){
+      int numPerguntas, String suaPatente, String[] patentes, String nome, int posicao) {
 
     if (alternativasCorretas[numPerguntas].equals(alternativaSelecionada)) {
       posicao++;
       suaPatente = promover(patentes, posicao);
-      System.out.printf("Parabens, voce realmente acertou %s. Agora eu vou te promover para a posicão de %s\n", nome, suaPatente);
-    } else{
-      System.out.printf("Que pena %s, mas não desanima, vamos para a proxima. Voce esta na patente %s\n", nome, suaPatente);
+      System.out.printf(
+          "Parabens, voce realmente acertou %s. Agora eu vou te promover para a posicão de %s. Vamos para a proxima pergunta!!\n",
+          nome,
+          suaPatente);
+    } else {
+      System.out.printf(
+          "Que pena %s, mas não desanima, vamos para a proxima. Voce esta na patente %s. Vamos para a proxima pergunta!!\n",
+          nome,
+          suaPatente);
     }
   }
 
@@ -118,7 +122,7 @@ public class ByteDoMilhao {
     falaInicial(nome, suaPatente);
     String[][] perguntasEAlternativas = criarPerguntasEAlternativas();
     String[] alternativasCorretas = criarAlternativasCorretas();
-    
+
     int numPerguntas = 0;
 
     while (true) {
@@ -129,7 +133,8 @@ public class ByteDoMilhao {
       imprimirPerguntaEAlternativas(perguntasEAlternativas, numPerguntas);
 
       String alternativaSelecionada = perguntaDaAlternativa(nome);
-      verificarSePerguntaCorreta(alternativaSelecionada, alternativasCorretas, numPerguntas, suaPatente, suaPatente, nome);
+      verificarSePerguntaCorreta(alternativaSelecionada, alternativasCorretas, numPerguntas, suaPatente, patentes, nome,
+          posicao);
 
       numPerguntas++;
 
