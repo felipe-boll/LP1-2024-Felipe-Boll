68f04b4ce5edce5c469276319bf18ee8d60fc244
felipe-boll <felipechavesboll@gmail.com>
2025-08-15 10:44:21 -0300

    TA PORRA, 35 atualizaçoes

---
+++ profRoberta/exemplo-oo/exemplo-oo/src/lista/No.java
@@ -0,0 +1,30 @@
+package lista;
+
+public class No {
+    private int elemento;
+    private No proximo;
+    
+    public No() {
+    }
+
+    public int getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(int elemento) {
+        this.elemento = elemento;
+    }
+
+    public No getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(No proximo) {
+        this.proximo = proximo;
+    }
+
+    
+
+    
+
+}
